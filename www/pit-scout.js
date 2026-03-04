a3544262eead492e871e87ab0b6be7e76981c778
Micah Krikke <258483616+MicahKrikke-85@users.noreply.github.com>
2026-03-03 19:16:43 -0500

    To be an apostrophe or quotation mark, that is the real question....

--- www/pit-scout.js
+++ www/pit-scout.js
@@ -447,6 +447,8 @@ addI18n({
 		fr:'Remarques (Autres informations utiles)',
 		zh_tw:'備註（其他值得了解的事）',
 	},
+	to_alliance_zone_question:{
+		en: "How does your robot get to the alliance zone?",
 	pit_scout_photo_reminder:{
 		en:'If you are also bot photo scouting, don\'t forget to take a picture!',
 		pt:'Se você também estiver explorando fotos de robôs, não se esqueça de tirar uma foto!',
