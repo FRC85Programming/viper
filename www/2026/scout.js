60f1fb72f234d2ae9f78d95862bf88b58d5a2b60
Micah Krikke <258483616+MicahKrikke-85@users.noreply.github.com>
2026-03-05 18:32:44 -0500

    is this it

--- www/2026/scout.js
+++ www/2026/scout.js
@@ -1007,57 +1007,7 @@ $(document).ready(function(){
 	promisePitScouting().then(function(data){
 		pitData = data
 	})
-
-	let autoCount = 0;
-	let teleCount = 0;
-
-	document.getElementById("autoFuelMinusOne").onclick = function(){
-		autoCount-=1;
-
-		document.getElementById("autoFuelScore").innerHTML = autoCount;
-	}
-
-	document.getElementById("autoFuelOne").onclick = function(){
-		autoCount+=1;
-
-		document.getElementById("autoFuelScore").innerHTML = autoCount;
-	}
-
-	document.getElementById("autoFuelFive").onclick = function(){
-		autoCount+=5;
-
-		document.getElementById("autoFuelScore").innerHTML = autoCount;
-	}
-
-	document.getElementById("autoFuelEight").onclick = function(){
-		autoCount+=8;
-
-		document.getElementById("autoFuelScore").innerHTML = autoCount;
-	}
-
-		document.getElementById("teleFuelMinusOne").onclick = function(){
-		teleCount-=1;
-
-		document.getElementById("teleFuelScore").innerHTML = teleCount;
-	}
-
-	document.getElementById("teleFuelOne").onclick = function(){
-		teleCount+=1;
-
-		document.getElementById("teleFuelScore").innerHTML = teleCount;
-	}
-
-	document.getElementById("teleFuelFive").onclick = function(){
-		teleCount+=5;
-
-		document.getElementById("teleFuelScore").innerHTML = teleCount;
-	}
-
-	document.getElementById("teleFuelTen").onclick = function(){
-		teleCount+=10;
-
-		document.getElementById("teleFuelScore").innerHTML = teleCount;
-	}
+	
 
 	$('.kebab-button').click(function(e){
 		var dropdown = $(this).siblings('.kebab-dropdown')
