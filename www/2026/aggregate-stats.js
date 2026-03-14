a13586d143fd8dea2d56c31d00cf46850cc56ae7
Micah Krikke <258483616+MicahKrikke-85@users.noreply.github.com>
2026-03-14 12:34:23 -0400

    Micah is MAD M for mad M in Micah stands for Mad
    gracie not mad

--- www/2026/aggregate-stats.js
+++ www/2026/aggregate-stats.js
@@ -2093,6 +2093,21 @@ function showSubjectiveScouting(el,team){
 	})
 }
 
+var importFunctions={
+	"195":{
+		example:"/2026/195.csv",
+		convert:importScouting195,
+	},
+	// "3061":{
+	// 	example:"/2025/3061.json",
+	// 	convert:importScouting3061,
+	// },
+	// "Lovat":{
+	// 	example:"/2025/lovat.tsv",
+	// 	convert:importScoutingLovat,
+	// },
+}
+
 function importScouting195(text){
 	var rows=csvToArrayOfMaps(text)
 	rows.forEach(row=>{
