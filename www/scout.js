c78d7d31329c8b9e107320ed3ba2538cc5ff8acd
Micah Krikke <258483616+MicahKrikke-85@users.noreply.github.com>
2026-03-23 17:33:16 -0400

    Revert "hmmm"
    This reverts commit 1738d0c64065aef6729235b14f4867388b9dd040.

--- www/scout.js
+++ www/scout.js
@@ -476,12 +476,11 @@ $(window).on('hashchange', function(){
 })
 
 function showSelectPitScoutTeam(){
-	Promise.allSettled([
+	Promise.all([
 		promiseEventTeams(),
 		promisePitScouting()
-	]).then(results=>{
-		var eventTeams = results[0].status=="fulfilled"?results[0].value:[],
-		pitData = results[1].status=="fulfilled"?results[1].value:{}
+	]).then(values=>{
+		var [eventTeams, pitData] = values
 		$('.screen,.init-hide').hide()
 		resetInitialValues(pitScouting)
 		setHash(null,null,null,null,teamList)
@@ -496,9 +495,6 @@ function showSelectPitScoutTeam(){
 		}
 		if ((!showTeams || !showTeams.length) && pitData){
 			showTeams = Object.keys(pitData).map(s=>parseInt(s)).filter(Number)
-		}
-		if (!showTeams || !showTeams.length){
-			showTeams = getLocalPitTeamsForEvent()
 		}
 		showTeams = [...new Set((showTeams||[]).filter(Number))].sort((a,b)=>a-b)
 		$('.location-pointer').remove()
@@ -1126,18 +1122,6 @@ function getTeamsWithPitData(){
 		}
 	}
 	return teams
-}
-
-function getLocalPitTeamsForEvent(){
-	var teams = {}
-	for (var i in localStorage){
-		var m = i.match(/^(?:uploaded_)?(.+)_([0-9]+)$/)
-		if (!m) continue
-		if (m[1] !== eventId) continue
-		var t = parseInt(m[2])
-		if (t) teams[t]=1
-	}
-	return Object.keys(teams).map(t=>parseInt(t))
 }
 
 function getTeamsWithSubjectiveData(){
