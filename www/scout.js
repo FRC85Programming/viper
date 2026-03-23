b34cce1d8c2bfbcdbd30334be5e6ffd28a277bfe
Micah Krikke <258483616+MicahKrikke-85@users.noreply.github.com>
2026-03-23 17:33:13 -0400

    Revert "where my dad... I all alone...."
    This reverts commit c872ad45245116724a61b62dbf063c8c6adbbe80.

--- www/scout.js
+++ www/scout.js
@@ -1158,13 +1158,6 @@ function pitScoutNext(uploaded){
 	return false
 }
 
-function goSelectPitTeam(){
-	localStorage.setItem("last_scout_action","team")
-	maybeSaveFirst()
-	showSelectPitScoutTeam()
-	return false
-}
-
 function subjectiveScoutNext(uploaded){
 	if (uploaded!="uploaded") localStorage.setItem("last_scout_action","next")
 	storeSubjectiveScouting(uploaded)
@@ -1449,8 +1442,7 @@ $(document).ready(function(){
 		$('body').append($('<img class=location-pointer src=/pointer.png style="position:absolute;width:3em">').css('top',e.pageY).css('left',e.pageX).addClass(name))
 		inputChanged(inp.val(val), `${x}%x${y}%`)
 	})
-	$("#nextBtn,#pitScoutNext,#subjectiveScoutNext,#subjectiveTeamButton").click(goNext)
-	$("#pitTeamButton").click(goSelectPitTeam)
+	$("#nextBtn,#pitScoutNext,#pitTeamButton,#subjectiveScoutNext,#subjectiveTeamButton").click(goNext)
 	$("#matchBtn").click(goChooseMatch)
 	$(".robotBtn").click(goChooseRobot)
 	$(".fieldRotateBtn").click(rotateField)
