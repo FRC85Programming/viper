1bc664f148660629518a8023eabdca0bf65f9e4c
Micah Krikke <258483616+MicahKrikke-85@users.noreply.github.com>
2026-03-24 19:03:41 -0400

    Revert "hWHAT"
    This reverts commit 367e926c68b1574e1d8bba356b7c85bfaba313c8.

--- www/pit-scout.js
+++ www/pit-scout.js
@@ -584,5 +584,5 @@ function updatePageContent(html, url) {
 	window.history.pushState({path: url}, '', url);
 }
 window.addEventListener('popstate', function(event) {
-	loadNewContent(window.location.href);
+	loadNewContent(window.location.pathname);
 });
